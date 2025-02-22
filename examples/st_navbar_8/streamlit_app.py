d6f031ec8a1c7d5c9181adbf69c751e5dee63aad
Hans Then <hans.then@gmail.com>
2025-02-22 19:14:42 +0100

    Allow st.Page objects in the input (#26)

---
+++ examples/st_navbar_8/streamlit_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from streamlit_navigation_bar import st_navbar
+
+def about():
+    st.write("call about page")
+
+about = st.Page(about, title="About")
+
+page = st_navbar(
+    ["Home", "Documentation", "Examples", "Account", about],
+    allow_reselect=True,
+    options= {
+        "use_padding": False,
+    },
+    key="navbar"
+)
+
+st.session_state.previous_page = page
+
+if page == about:
+    page.run()
+else:
+    st.write(page)
+
+
