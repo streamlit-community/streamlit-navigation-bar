ce7f322a6b048936b1a752c8d702f085f0af7450
Hans Then <hans.then@gmail.com>
2025-02-23 20:25:38 +0100

    Fail the build

--- examples/st_navbar_1/streamlit_app.py
+++ examples/st_navbar_1/streamlit_app.py
@@ -2,5 +2,6 @@ import streamlit as st
 from streamlit_navigation_bar import st_navbar
 
 
-page = st_navbar(["Home", "Documentation", "Examples", "Community", "About"])
+page = st_navbar(["A", "B", "C", "D", "E"])
 st.write(page)
+st.write("This should fail")
