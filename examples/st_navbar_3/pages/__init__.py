c6a112e90691db807f8d9d9b9c8392336ec703c1
Hans Then <hans.then@gmail.com>
2025-02-23 16:07:17 +0100

    Fix errors introduced by ruff

---
+++ examples/st_navbar_3/pages/__init__.py
@@ -0,0 +1,15 @@
+from pages.home import show_home
+from pages.install import show_install
+from pages.user_guide import show_user_guide
+from pages.api import show_api
+from pages.examples import show_examples
+from pages.community import show_community
+
+__all__ = [
+    "show_home",
+    "show_install",
+    "show_user_guide",
+    "show_api",
+    "show_examples",
+    "show_community",
+]
--- tests/test_release.py
+++ tests/test_release.py
@@ -1,4 +1,4 @@
 def test_release():
-    from streamlit_folium import _RELEASE
+    from streamlit_navigation_bar import _RELEASE
 
     assert _RELEASE, "Release needs to be set to True after finished testing locally"
