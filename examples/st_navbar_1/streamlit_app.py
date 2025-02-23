293546f7e22d10c521cd638dbc9fb477c1ac3cb0
Hans Then <hans.then@gmail.com>
2025-02-23 20:16:43 +0100

    Try again: should fail

--- examples/st_navbar_1/streamlit_app.py
+++ examples/st_navbar_1/streamlit_app.py
@@ -5,4 +5,4 @@ from streamlit_navigation_bar import st_navbar
 page = st_navbar(["*", "*", "*", "*", "*"])
 st.write(page)
 
-"This should break the visual tests again"
+"This should break the visual tests again 1"
