e4a51138e932c615e04e87909c18672db198420e
Hans Then <hans.then@gmail.com>
2025-02-23 11:59:33 +0100

    Break the build

--- examples/st_navbar_1/streamlit_app.py
+++ examples/st_navbar_1/streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 from streamlit_navigation_bar import st_navbar
 
 
-page = st_navbar(["Home", "Documentation", "Examples", "Community", "About"])
+page = st_navbar(["*", "*", "*", "*", "*"])
 st.write(page)
